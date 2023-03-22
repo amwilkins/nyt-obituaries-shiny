ae331c0630214002035441a2d3e7415b7b279363
amwilkins <awilkins72@gmail.com>
2023-03-22 10:33:09 -0500

    first setup

---
+++ App/app.R
@@ -0,0 +1,101 @@
+# Austin Wilkins
+# March 7, 2022
+
+# Libraries
+library(tidyverse)
+library(lubridate)
+library(shiny)
+library(data.table)
+library(ggthemes)
+
+options(scipen = 10)
+
+df <- fread("df.csv")
+yearly <- fread("yearly.csv")
+
+#Functions
+
+#   # basic count plot
+termPlot <- function(x){
+  df %>% 
+    select(Year,x) %>%
+    rename('n'= x) %>%
+    group_by(Year) %>%
+    summarize(n=sum(n)) %>%
+    inner_join(yearly)
+}
+
+#   # Ratio of words to total words
+ratioPlot <- function(x){
+  df %>% 
+    select(Year,x) %>%
+    rename('n'= x) %>%
+    group_by(Year) %>%
+    summarize(n=sum(n)) %>%
+    inner_join(yearly) %>% 
+    mutate(n = n/Total)
+} 
+
+
+ui <- fluidPage(
+
+
+    titlePanel("New York Times Obituaries"),
+
+    
+    sidebarLayout(
+        sidebarPanel(
+          selectInput("textInput", "Select a word", names(df[1,5:(length(df)-2)])),
+          selectInput("y", "Y axis", c("rate", "count")),
+          tableOutput('table1')
+        ),
+
+        mainPanel(
+           plotOutput("plot1"),
+           plotOutput("plot2")
+        )
+    )
+)
+
+
+
+server <- function(input, output, session) {
+
+
+  plotObj <- reactive({
+    if (input$y == "count") {
+      termPlot(input$textInput)
+    } else {
+      ratioPlot(input$textInput)
+    }
+  })
+
+  output$table1 <- renderTable({
+    plotObj()
+  }) 
+  
+  
+  
+  
+  # Reference Plot
+  output$plot1 <- renderPlot({
+    df %>% 
+      ggplot(aes(x=Year))+
+      geom_bar()+
+      labs(title='NYT Obituaries per Year')+
+      theme_fivethirtyeight()
+  })
+  
+  # Changing term plot
+  output$plot2 <- renderPlot({
+    plotObj() %>% 
+      ggplot(aes(x=Year,y=n))+
+      geom_col()+
+      geom_smooth(se=F,method='lm')+
+      theme_fivethirtyeight()
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM rocker/shiny:4.0.5
+
+# Install system requirements for index.R as needed
+RUN apt-get update && apt-get install -y \
+    --no-install-recommends \
+    git-core \
+    libssl-dev \
+    libcurl4-gnutls-dev \
+    curl \
+    libsodium-dev \
+    libxml2-dev \
+    libicu-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN install2.r --error --skipinstalled \
+    shiny \
+    lubridate \
+    data.table \
+    ggplot2 \
+    ggthemes 
+
+COPY ./App/* /srv/shiny-server/
+
+USER shiny
+
+EXPOSE 3838
+
+CMD ["/usr/bin/shiny-server"]

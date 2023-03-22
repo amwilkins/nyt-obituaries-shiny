d3f5f7b3e4d8ecf85673b72f2b7ce32fc46736ad
amwilkins <awilkins72@gmail.com>
2023-03-22 15:27:10 -0500

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+
+# Decode SSH key
+echo "${ACTION_KEY}" > ssh_key
+chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
+
+
+echo "Deploying via remote SSH"
+ssh -i ssh_key "root@${SERVER_HOSTNAME}" \
+  "docker pull amwilkins/obit:latest \
+  && docker stop obit \
+  && docker rm obit \
+  && docker run -d --name obit -p 3838:3838 amwilkins/obit:latest \
+  && docker system prune -af" # remove unused images to free up space

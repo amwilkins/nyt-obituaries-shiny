11aeef58fe2139a8e0de746301429b999f78e8d0
Austin Michael Wilkins <42476341+amwilkins@users.noreply.github.com>
2023-03-22 17:09:48 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 echo "${ACTION_KEY}" | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
+cat ssh_key
+
 
 echo "Deploying via remote SSH"
-ssh -i ssh_key 'root@74.208.91.172' \
+ssh -i ssh_key "root@74.208.91.172" \
   "docker pull amwilkins/obit:latest \
   && docker stop obit \
   && docker rm obit \
